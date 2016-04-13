52a3e1b73393d882d383c4eef1aacecf0cb08c7f
bhargavvi-dev <bhargavp.sheltersoft@gmail.com>
2016-04-13 18:48:32 +0530

    add smpt in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,17 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.action_mailer.default_url_options = {:host => "crop-monitor-system.herokuapp.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "ptlbhargav93",
+    password: "india@786"
+  }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
+
 import requests
-#url = 'https://crop-monitor-system.herokuapp.com/readings'
-url = 'http://localhost:3000/readings'
-payload = {'temp_read': '65', 'humi_read': '30', 'sensor_id':'2'}
+url = 'http://crop-monitor-system.herokuapp.com/readings'
+#url = 'http://localhost:3000/readings'
+payload = {'temp_read': '255', 'humi_read': '30', 'sensor_id':'2'}
 r = requests.post(url, params=payload)
 print(r.url)
