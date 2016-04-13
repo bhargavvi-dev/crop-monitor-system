c85eda791fb8017bb4995b56b8c8b25f792aafbb
bhargavvi-dev <bhargavp.sheltersoft@gmail.com>
2016-04-13 18:57:26 +0530

    test

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 
 import requests
-url = 'http://crop-monitor-system.herokuapp.com/readings'
+url = 'https://crop-monitor-system.herokuapp.com/readings'
 #url = 'http://localhost:3000/readings'
-payload = {'temp_read': '255', 'humi_read': '30', 'sensor_id':'2'}
+payload = {'temp_read': '355', 'humi_read': '30', 'sensor_id':'2'}
 r = requests.post(url, params=payload)
 print(r.url)
